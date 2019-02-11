8c74041a642eda4a2c315d4390147c51d2da3cbb
Domenico Posillipo <domposi@icloud.com>
2019-02-11 19:26:35 +0100

    Initial Commit

---
+++ SoccerBox/Lista_TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  Lista_TableViewCell.swift
+//  SoccerBox
+//
+//  Created by Domenico Posillipo on 11/02/2019.
+//  Copyright © 2019 Domenico Posillipo. All rights reserved.
+//
+
+import UIKit
+
+class Lista_TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ SoccerBox/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SoccerBox
+//
+//  Created by Domenico Posillipo on 08/02/2019.
+//  Copyright © 2019 Domenico Posillipo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        UIApplication.shared.statusBarStyle = .lightContent
+    }
+
+
+}
+
