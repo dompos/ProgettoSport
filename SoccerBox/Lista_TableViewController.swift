ee4a66600917cb6011b51076ce458a78d3b9b0ff
Marco Ragozzino <marcoragozzino@cam-31.local>
2019-02-12 09:56:18 +0100

    Aggiunto commento

--- SoccerBox/Lista_TableViewController.swift
+++ SoccerBox/Lista_TableViewController.swift
@@ -13,7 +13,7 @@ class Lista_TableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        //ajshvajshkdbasd
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
